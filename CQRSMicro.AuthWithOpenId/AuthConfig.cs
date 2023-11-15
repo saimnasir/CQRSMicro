c2ee983d007364f2ff5fc1601bb755f48fb2cae1
Saim Nasır <saim.nasir@patikaglobal.com>
2023-11-15 23:40:01 +0300

    auth with open id - trial

---
+++ CQRSMicro.AuthWithOpenId/AuthConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace CQRSMicro.AuthWithOpenId
+{
+    public static class AuthConfig
+    {
+        public static string Authority => "https://localhost:6001";
+        public static string ClientId => "auth-app";
+        public static string ClientSecret => "PW5BQUmVuXxU7Muw7AVWyFhKWx9ujtYh";
+    }
+}
---
+++ CQRSMicro.AuthWithOpenId/Startup.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using System.Text;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace CQRSMicro.AuthWithOpenId
+{
+    public class Startup
+    {
+         public IConfiguration Configuration { get; } 
+        private IWebHostEnvironment Environment { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore().AddApiExplorer();
+            AddAuthentication(services);
+            AddServices(services);
+            SetupCORS(services);
+        }
+
+        private void AddAuthentication(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                .AddCookie()
+                .AddOpenIdConnect(options =>
+                {
+                    options.Authority = AuthConfig.Authority;
+                    options.ClientId = AuthConfig.ClientId;
+                    options.ClientSecret = AuthConfig.ClientSecret;
+                    options.CallbackPath = "/signin-oidc";
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.SaveTokens = true;
+                    // Configure additional options as needed
+                    options.Events = new OpenIdConnectEvents
+                    {
+                        OnRedirectToIdentityProvider = context =>
+                        {
+                            // Your logic before redirecting to the identity provider
+                            return Task.CompletedTask;
+                        },
+                        OnTokenValidated = context =>
+                        {
+                            // Your logic after the token is validated
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+        }
+
+        private void AddServices(IServiceCollection services)
+        { 
+            services.AddControllers();
+            services
+              .AddControllersWithViews()
+              .AddNewtonsoftJson(options => options.SerializerSettings.Converters.Add(new StringEnumConverter()));
+
+            services.AddRouting(o => o.LowercaseUrls = true);
+            AddSwagger(services);
+
+            //services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());                        
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+          
+        }
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGenNewtonsoftSupport();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    BearerFormat = "JWT",
+                    Name = "Authorization",
+                    Description = "Example: \"Bearer {token}\"",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+                c.DocInclusionPredicate((name, api) => true);
+                c.TagActionsBy(api =>
+                {
+                    if (api.GroupName != null)
+                    {
+                        return new[] { api.GroupName };
+                    }
+
+                    if (api.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+                    {
+                        return new[] { controllerActionDescriptor.ControllerName };
+                    }
+
+                    throw new InvalidOperationException("Unable to determine tag for endpoint.");
+                });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "OpenId Auth",
+                    Description = "OpenId Auth"
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+        }
+     
+
+        private static void SetupCORS(IServiceCollection services)
+        {
+            services.AddCors(opts =>
+            {
+                opts.AddPolicy("corsapp", policy =>
+                {
+                    policy.WithOrigins("*")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    //.AllowCredentials()
+                    ;
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        { 
+            //if (env.IsDevelopment())
+            //{
+            app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseForwardedHeaders();
+            
+            UseSwagger(app, env);
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors("corsapp");
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            { 
+                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+
+
+        private static void UseSwagger(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer Api");
+                options.RoutePrefix = string.Empty;
+            });
+
+        }
+    }
+}
